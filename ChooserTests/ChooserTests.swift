3fb8a76b378a0612a7b5287d63cf4feaa0d8e310
pepito2293 <pepito.gaming22@gmail.com>
2026-03-31 22:27:45 +0200

    Initial Commit

---
+++ ChooserTests/ChooserTests.swift
@@ -0,0 +1,18 @@
+//
+//  ChooserTests.swift
+//  ChooserTests
+//
+//  Created by Maël DUPIN on 31/03/2026.
+//
+
+import Testing
+
+struct ChooserTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
